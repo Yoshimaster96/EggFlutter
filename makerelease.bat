4aa756d0a2e9bf379be97ebcdc0b848c3866fca3
Yoshimaster96 <yoshimaster96smwc@gmail.com>
2021-07-22 14:43:41 -0500

    Fix some formatting

---
+++ makerelease.bat
@@ -0,0 +1,12 @@
+@echo off
+
+rem	===============================
+rem	Copy files to release directory
+rem	===============================
+	copy Project2.exe release\EggFlutter.exe
+rem	copy resources\help\Project2.chm release\EggFlutter.chm
+
+rem	==============================
+rem	Pause so we can see any errors
+rem	==============================
+	pause
--- resources/patches/makepatch_8mb.bat
+++ resources/patches/makepatch_8mb.bat
@@ -1,10 +1,23 @@
 @echo off
+
+rem	=============================
+rem	If ROM exists, generate patch
+rem	=============================
 if exist yi.sfc (
 	copy yi.sfc yi_patched.sfc
 	asar main_8mb.asm yi_patched.sfc
 	beat -create -delta -p patch_8mb.bps -o yi_patched.sfc yi.sfc
 	del yi_patched.sfc
-) else (
+)
+
+rem	===============================================
+rem	Otherwise, let user know that ROM doesn't exist
+rem	===============================================
+else (
 	echo Could not find Yoshi's Island US 1.0 ROM `yi.sfc`.
 )
+
+rem	==============================
+rem	Pause so we can see any errors
+rem	==============================
 pause
